851c118226a14d5c8cbadbedc5f4eaaec98e462f
kumarshubhh <subhanshukumar290@gmail.com>
2024-11-15 13:36:06 +0530

    update files

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.get("/posts/:id/edit", (req, res) =>{
     let post =posts.find((p) =>id === p.id);
     res.render("edit.ejs", { post });
 });
-app.delete("/post/:id", (req,res) =>{
+app.delete("/posts/:id", (req,res) =>{
     let {id} = req.params;
      posts =posts.filter((p) =>id !== p.id);
     res.redirect("/posts");
